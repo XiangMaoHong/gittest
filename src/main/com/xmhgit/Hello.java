cf34ec0f32102da535e2feab884390baff598344
xmh <2398458233@qq.com>
2022-11-10 14:15:36 +0800

    创建了项目

---
+++ src/main/com/xmhgit/Hello.java
@@ -0,0 +1,11 @@
+package main.com.xmhgit;
+
+/**
+ * @author xmh
+ * @date 2022/11/10
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
